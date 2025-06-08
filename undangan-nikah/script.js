da69cbe30a0d452f256d05f4737cbe950b32456f
Yadi301 <keyraucil24@gmail.com>
2025-06-08 11:02:08 +0700

    Delete undangan-nikah directory

--- undangan-nikah/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  const params = new URLSearchParams(window.location.search);
-  const nama = params.get("dear");
-  const namaTamu = document.getElementById("namaTamu");
-  if (nama) {
-    namaTamu.textContent = decodeURIComponent(nama);
-  }
-};
